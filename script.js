c42e3ecb65e7446afda3d3e544cec93403b678e0
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-19 12:55:51 +0000

    comments added

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+// The line below allows the button to call the function when clicked
 document.getElementById('rec').addEventListener('click', recommendation)
 
+// The function below checks if the user's age 
 function recommendation () {
+  // This stores the input as the variable age
   let age = document.getElementById('age').value
+  /* The if statements below check if the age is greater than the requirements to watch the movies. The recommendations will pop up as well if the condition is met. */
   if (age >= 17) {
     document.getElementById('result').innerHTML = 'You can see R rated Movies'
   } else if (age >= 13) {
@@ -11,5 +15,7 @@ function recommendation () {
   } else {
     document.getElementById('result').innerHTML = 'You are probably too young to watch movies'
   }
-  document.getElementById('thanks').innerHTML = "Thank you! For being honest and verifying your age"
+
+//  The line below thanks the user for inputting their age
+document.getElementById('thanks').innerHTML = "Thank you! For being honest and verifying your age"
 }
